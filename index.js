e90e6414f7103edb709d6ca93ba9590fd0f80799
AlexMaster168 <alex4185@ukr.net>
2022-12-04 22:29:34 +0200

    update bot

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const TelegramBot = require("node-telegram-bot-api")
+﻿const TelegramBot = require("node-telegram-bot-api")
+const express = require('express');
+const cors = require('cors');
 
 const token = "Your Telegramm token"
-const bot = new TelegramBot(token, {polling: true})
 const webAppUrl = "https://telegram-client-bot.netlify.app/"
+const bot = new TelegramBot(token, {polling: true})
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 bot.on('message', async msg => {
     const chatId = msg.chat.id
@@ -12,7 +18,7 @@ bot.on('message', async msg => {
        await bot.sendMessage(chatId,'Ниже появится кнопка заполни форму', {
            reply_markup: {
                keyboard: [
-                   [{text: "Заполнить форму", web_app: {url: webAppUrl}}]
+                   [{text: "Заполнить форму", web_app: {url: webAppUrl + "/form"}}]
                ]
            }
        })
@@ -25,4 +31,39 @@ bot.on('message', async msg => {
             }
         })
     }
+
+    if(msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
+            await bot.sendMessage(chatId, 'Ваш номер телефона: ' + data?.phone);
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
+        }
+    }
 })
+app.post('/web-data', async (req, res) => {
+    const {queryId, products = [], totalPrice} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успешная покупка',
+            input_message_content: {
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+            }
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        return res.status(500).json({})
+    }
+})
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
